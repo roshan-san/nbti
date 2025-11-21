a5b16121242dcf3d5250c4ec75bc11e9c48295e7
roshanjeyarubanr <roshanjeyarubanr@gmail.com>
2025-11-21 19:00:58 +0530

    rmv contracts

--- packages/server/src/routes/test.route.ts
+++ packages/server/src/routes/test.route.ts
@@ -1,9 +1,9 @@
-import type { contracts } from "@nbti/shared";
+// import type { contracts } from "@nbti/shared";
 import { Hono } from "hono";
 
 export const testRoute = new Hono()
     .get("/", async (c) => {
-        return c.json<contracts.SuccessResponse>({
+        return c.json({
             success: true,
             message: "Test route is working!",
         });
